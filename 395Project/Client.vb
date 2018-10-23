5aa1bda105f2106a3e5fcb2dd6de1b65b7ba5b31
LeonXiao587 <LeonXiao587@gmail.com>
2018-10-22 23:10:20 -0600

    gridview update and delete

--- 395Project/Client.vb
+++ 395Project/Client.vb
@@ -1,4 +1,5 @@
-﻿Public Class Client
+﻿Imports System.Data.SqlClient
+Public Class Client
     Public Sub LoadGrid()
         Dim idsearch As Integer
         idsearch = 0
@@ -7,7 +8,7 @@
         If (IsNumeric(searchbox.Text)) Then
             idsearch = searchbox.Text
         End If
-
+        ' stored proc will not compatible with the gridview data update and delete!!! May have to use simpler way
         login.SQL.ExecQuery("searchClient " + idsearch.ToString + "," + searchstr)
         DataGridView1.DataSource = login.SQL.DBDS.Tables(0)
     End Sub
@@ -19,4 +20,24 @@
 
     End Sub
 
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim intResult As Integer
+        intResult = MessageBox.Show("You sure you want to make these changes", "Waring", MessageBoxButtons.OKCancel, MessageBoxIcon.Asterisk, MessageBoxDefaultButton.Button1)
+        If intResult = DialogResult.OK Then
+            Dim scb As SqlCommandBuilder = New SqlCommandBuilder(login.SQL.DBDA)
+            login.SQL.DBDA.Update(login.SQL.DBDS)
+            MsgBox("Changes updated!")
+        End If
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim intResult As Integer
+        intResult = MessageBox.Show("You sure you want to cancel this order", "Waring", MessageBoxButtons.OKCancel, MessageBoxIcon.Asterisk, MessageBoxDefaultButton.Button1)
+        If intResult = DialogResult.OK Then
+            DataGridView1.Rows.RemoveAt(DataGridView1.CurrentCell.RowIndex)
+            Dim scb As SqlCommandBuilder = New SqlCommandBuilder(login.SQL.DBDA)
+            login.SQL.DBDA.Update(login.SQL.DBDS)
+            MsgBox("Deleted")
+        End If
+    End Sub
 End Class
