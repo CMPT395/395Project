7af64fb9dd8c55a7321dcc3cd34fc9cc9af665f6
LeonXiao587 <LeonXiao587@gmail.com>
2018-10-28 23:48:52 -0600

    close to hide

--- 395Project/Client.vb
+++ 395Project/Client.vb
@@ -60,7 +60,7 @@ Public Class Client
     End Sub
 
     Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
-        Me.Close()
+        Me.Hide()
         NewClient.Show()
 
     End Sub
