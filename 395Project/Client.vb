adfdb705e85b7cf47dd1ff4febfe8c18080e3bb6
LeonXiao587 <leonxiao587@gmail.com>
2018-10-31 08:27:46 -0600

    wild card search

--- 395Project/Client.vb
+++ 395Project/Client.vb
@@ -10,7 +10,7 @@ Public Class Client
         End If
         ' stored proc will not compatible with the gridview data update and delete!!! May have to use simpler way
         'login.SQL.ExecQuery("searchClient " + idsearch.ToString + "," + searchstr)
-        login.SQL.ExecQuery("select * from Client where CLientID = " + idsearch.ToString + "or Name = '" + searchstr + "' or Pemail = '" + searchstr + "'")
+        login.SQL.ExecQuery("select * from Client where CLientID = " + idsearch.ToString + "or Name like '%" + searchstr + "%' or Pemail like '%" + searchstr + "%'")
         DataGridView1.DataSource = login.SQL.DBDS.Tables(0)
     End Sub
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
@@ -23,21 +23,15 @@ Public Class Client
 
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
         Dim intResult As Integer
-        intResult = MessageBox.Show("Are you sure you want to make these changes", "Waring", MessageBoxButtons.OKCancel, MessageBoxIcon.Asterisk, MessageBoxDefaultButton.Button1)
-        If intResult = DialogResult.OK Then
-            Dim scb As SqlCommandBuilder = New SqlCommandBuilder(login.SQL.DBDA)
-            login.SQL.DBDA.Update(login.SQL.DBDS)
-            MsgBox("Changes updated!")
 
-            If (DataGridView1.SelectedCells.Count > 0) Then
-                intResult = MessageBox.Show("You sure you want to make these changes", "Waring", MessageBoxButtons.OKCancel, MessageBoxIcon.Asterisk, MessageBoxDefaultButton.Button1)
-                If intResult = DialogResult.OK Then
-                    Dim aaa As SqlCommandBuilder = New SqlCommandBuilder(login.SQL.DBDA)
-                    login.SQL.DBDA.Update(login.SQL.DBDS)
-                    MsgBox("Changes updated!")
-                Else
-                    MessageBox.Show("Please make a selection before removing")
-                End If
+        If (DataGridView1.SelectedCells.Count > 0) Then
+            intResult = MessageBox.Show("You sure you want to make these changes", "Waring", MessageBoxButtons.OKCancel, MessageBoxIcon.Asterisk, MessageBoxDefaultButton.Button1)
+            If intResult = DialogResult.OK Then
+                Dim aaa As SqlCommandBuilder = New SqlCommandBuilder(login.SQL.DBDA)
+                login.SQL.DBDA.Update(login.SQL.DBDS)
+                MsgBox("Changes updated!")
+            Else
+                MessageBox.Show("Please make a selection before removing")
             End If
         End If
 
@@ -45,25 +39,17 @@ Public Class Client
 
     Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
         Dim intResult As Integer
-        intResult = MessageBox.Show("Delete this Client?", "Waring", MessageBoxButtons.OKCancel, MessageBoxIcon.Asterisk, MessageBoxDefaultButton.Button1)
-        If intResult = DialogResult.OK Then
-            DataGridView1.Rows.RemoveAt(DataGridView1.CurrentCell.RowIndex)
-            Dim scb As SqlCommandBuilder = New SqlCommandBuilder(login.SQL.DBDA)
-            login.SQL.DBDA.Update(login.SQL.DBDS)
-            MsgBox("Deleted")
-
-            If (DataGridView1.SelectedCells.Count > 0) Then
-                intResult = MessageBox.Show("You sure you want to cancel this order", "Warning", MessageBoxButtons.OKCancel, MessageBoxIcon.Asterisk, MessageBoxDefaultButton.Button1)
-                If intResult = DialogResult.OK Then
-                    DataGridView1.Rows.RemoveAt(DataGridView1.CurrentCell.RowIndex)
-                    Dim bbb As SqlCommandBuilder = New SqlCommandBuilder(login.SQL.DBDA)
-                    login.SQL.DBDA.Update(login.SQL.DBDS)
-                    MsgBox("Deleted")
-                Else
-                    MessageBox.Show("Please make a selection before removing")
-                End If
-
+        If (DataGridView1.SelectedCells.Count > 0) Then
+            intResult = MessageBox.Show("You sure you want to cancel this order", "Warning", MessageBoxButtons.OKCancel, MessageBoxIcon.Asterisk, MessageBoxDefaultButton.Button1)
+            If intResult = DialogResult.OK Then
+                DataGridView1.Rows.RemoveAt(DataGridView1.CurrentCell.RowIndex)
+                Dim bbb As SqlCommandBuilder = New SqlCommandBuilder(login.SQL.DBDA)
+                login.SQL.DBDA.Update(login.SQL.DBDS)
+                MsgBox("Deleted")
+            Else
+                MessageBox.Show("Please make a selection before removing")
             End If
+
         End If
     End Sub
 
