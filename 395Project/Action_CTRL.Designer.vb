7418de8c1ade278aee486df799d82b1d3a855b58
LeonXiao587 <LeonXiao587@gmail.com>
2018-11-05 17:19:54 -0700

    email sending done
    button on hourtracking will do the email sending, need more other functions

--- 395Project/Action_CTRL.Designer.vb
+++ 395Project/Action_CTRL.Designer.vb
@@ -27,15 +27,16 @@ Partial Class Action_CTRL
         Me.Label2 = New System.Windows.Forms.Label()
         Me.Constructors = New System.Windows.Forms.Button()
         Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'Employers
         '
         Me.Employers.Font = New System.Drawing.Font("Times New Roman", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Employers.Location = New System.Drawing.Point(203, 162)
-        Me.Employers.Margin = New System.Windows.Forms.Padding(7, 5, 7, 5)
+        Me.Employers.Location = New System.Drawing.Point(186, 142)
+        Me.Employers.Margin = New System.Windows.Forms.Padding(6, 4, 6, 4)
         Me.Employers.Name = "Employers"
-        Me.Employers.Size = New System.Drawing.Size(249, 53)
+        Me.Employers.Size = New System.Drawing.Size(228, 46)
         Me.Employers.TabIndex = 3
         Me.Employers.Text = "Manage Client"
         Me.Employers.UseVisualStyleBackColor = True
@@ -43,10 +44,10 @@ Partial Class Action_CTRL
         'back
         '
         Me.back.Font = New System.Drawing.Font("Times New Roman", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.back.Location = New System.Drawing.Point(41, 37)
-        Me.back.Margin = New System.Windows.Forms.Padding(7, 5, 7, 5)
+        Me.back.Location = New System.Drawing.Point(38, 32)
+        Me.back.Margin = New System.Windows.Forms.Padding(6, 4, 6, 4)
         Me.back.Name = "back"
-        Me.back.Size = New System.Drawing.Size(151, 43)
+        Me.back.Size = New System.Drawing.Size(138, 38)
         Me.back.TabIndex = 4
         Me.back.Text = "Sign Out"
         Me.back.UseVisualStyleBackColor = True
@@ -54,20 +55,20 @@ Partial Class Action_CTRL
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(407, 45)
-        Me.Label2.Margin = New System.Windows.Forms.Padding(7, 0, 7, 0)
+        Me.Label2.Location = New System.Drawing.Point(373, 39)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(6, 0, 6, 0)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(154, 24)
+        Me.Label2.Size = New System.Drawing.Size(142, 21)
         Me.Label2.TabIndex = 7
         Me.Label2.Text = "Manager name"
         '
         'Constructors
         '
         Me.Constructors.Font = New System.Drawing.Font("Times New Roman", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Constructors.Location = New System.Drawing.Point(203, 239)
-        Me.Constructors.Margin = New System.Windows.Forms.Padding(7, 5, 7, 5)
+        Me.Constructors.Location = New System.Drawing.Point(186, 209)
+        Me.Constructors.Margin = New System.Windows.Forms.Padding(6, 4, 6, 4)
         Me.Constructors.Name = "Constructors"
-        Me.Constructors.Size = New System.Drawing.Size(249, 47)
+        Me.Constructors.Size = New System.Drawing.Size(228, 41)
         Me.Constructors.TabIndex = 8
         Me.Constructors.Text = "Manage Contractors"
         Me.Constructors.UseVisualStyleBackColor = True
@@ -75,25 +76,35 @@ Partial Class Action_CTRL
         'Button1
         '
         Me.Button1.Font = New System.Drawing.Font("Times New Roman", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Button1.Location = New System.Drawing.Point(203, 314)
-        Me.Button1.Margin = New System.Windows.Forms.Padding(7, 6, 7, 6)
+        Me.Button1.Location = New System.Drawing.Point(186, 275)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(6, 5, 6, 5)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(249, 49)
+        Me.Button1.Size = New System.Drawing.Size(228, 43)
         Me.Button1.TabIndex = 9
         Me.Button1.Text = "View Contract"
         Me.Button1.UseVisualStyleBackColor = True
         '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(248, 346)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(92, 34)
+        Me.Button2.TabIndex = 10
+        Me.Button2.Text = "Remainder"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
         'Action_CTRL
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(12.0!, 24.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(11.0!, 21.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(679, 520)
+        Me.ClientSize = New System.Drawing.Size(622, 455)
+        Me.Controls.Add(Me.Button2)
         Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.Constructors)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.back)
         Me.Controls.Add(Me.Employers)
-        Me.Margin = New System.Windows.Forms.Padding(7, 5, 7, 5)
+        Me.Margin = New System.Windows.Forms.Padding(6, 4, 6, 4)
         Me.Name = "Action_CTRL"
         Me.Text = "ManagerAction"
         Me.ResumeLayout(False)
@@ -105,4 +116,5 @@ Partial Class Action_CTRL
     Friend WithEvents Label2 As Label
     Friend WithEvents Constructors As Button
     Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
 End Class
---
+++ 395Project/HourTracking.vb
@@ -0,0 +1,42 @@
+﻿Imports System.Net.Mail
+
+Public Class HourTracking
+    Dim ReceiverAddressList As New List(Of String) From {
+            "leonxiao587@gmail.com"
+        }
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim Emailmessage As New MailMessage
+        Dim smtp As New SmtpClient
+
+        smtp.Host = "smtp.gmail.com"
+        smtp.UseDefaultCredentials = False
+        smtp.Port = 587
+        smtp.EnableSsl = True
+        smtp.Credentials = New System.Net.NetworkCredential("monthlyhourcollector@gmail.com", "cmpt395test")
+        Emailmessage.From = New MailAddress("leonxiao587@gmail.com")
+        'add Receiver
+        Emailmessage.To.Add("xiaoj3@mymacewan.ca")
+        'put body and subject for email here
+        Emailmessage.Subject = "Monthly Hour Required"
+        Emailmessage.Body = "Please click On the link To fill your working hours"
+
+        Try
+            smtp.Send(Emailmessage)
+            MessageBox.Show("Email Send！")
+
+        Catch
+            MessageBox.Show("Failed！")
+        Finally
+            Emailmessage.Dispose()
+        End Try
+        'ReceiverAddressList.Add("xiaoj3@mymacewan.ca")
+        'test = SendMail(ReceiverAddressList, "Monthly Hour Required", "Please click On the link To fill your working hours")
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Me.Close()
+
+    End Sub
+End Class
